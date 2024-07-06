f1925a02858ac3344e1637a5da9d1e507848398d
Temirlan <temabeu2003@gmail.com>
2024-07-06 22:59:46 +0500

    fix: fix product's unique identifier _id

--- controllers/productController.js
+++ controllers/productController.js
@@ -78,7 +78,7 @@ const deleteProduct = async (req, res) => {
             return res.status(404).json({ message: 'Product not found' });
         }
 
-        await product.remove();
+        await product.deleteOne();
         res.json({ message: 'Product removed' });
     } catch (err) {
         res.status(500).json({ message: err.message });
